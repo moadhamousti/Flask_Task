63cc3f33218ca59647df1e8f2159a4f35d046801
moadhamousti <118165767+moadhamousti@users.noreply.github.com>
2023-06-19 22:22:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -561,4 +561,4 @@ def fetch_repeated_tasks_from_database(user_id, limit):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True,host='0.0.0.0')
+    socketio.run(app, debug=True)
